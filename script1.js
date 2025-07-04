afef73048e4fc7eccba4699e16bbbd763264eba1
ManuM-5733 <manuvvp5@gmail.com>
2025-07-04 12:47:21 +0530

    Add files via upload
    full form

---
+++ script1.js
@@ -0,0 +1,15 @@
+function submit(){
+    debugger
+var fname=document.getElementById('fname')
+var lname=document.getElementById('lname')
+var fullname=fname.value+" "+lname.value
+console.log(fullname)
+var Email=document.getElementById('EMAIL')
+console.log(Email.value)
+var password=document.getElementById('password')
+console.log(password.value)
+var phno=document.getElementById('phno')
+console.log(phno.value)
+
+
+}
