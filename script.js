24e47a6bb770fb7968402b0015768c99b3b69422
ManuM-5733 <manuvvp5@gmail.com>
2025-07-24 13:38:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function submit() {
+    debugger;
+    var fullname = document.getElementById('fullname');
+    console.log(fullname.value);
+
+    var phno = document.getElementById('phno');
+    console.log(phno.value);
+
+    var email = document.getElementById('email');
+    console.log(email.value);
+
+    var feedback = document.getElementById("Feedback").value;
+    console.log(feedback)
+  
+   
+}
+
